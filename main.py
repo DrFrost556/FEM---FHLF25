c60e2858374fa33d9c01ab038c9ae2ab7135510c
DrFrost556 <77743641+DrFrost556@users.noreply.github.com>
2024-05-07 13:08:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ problem = BatteryProblem(0.03)
 
 vis_mesh(problem)
 
-a_stat, _, _ = problem.solve_static(1)
+a_stat, _, _ = problem.solve_static(600)
 vis_temp(a_stat, problem)
 
-snapshots, snapshot_time = problem.solve_transient()
-vis_transient(snapshots, snapshot_time, problem)
+snapshots, snapshot_time, deviations, deviation_time = problem.solve_transient()
+vis_transient(snapshots, snapshot_time, problem, deviations, deviation_time)
